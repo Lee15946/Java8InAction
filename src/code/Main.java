0501cd145055adaa1e37c8d6eb1c235be41a0d6e
yuhua.li <yuhua.li@thoughtworks.com>
2022-10-08 18:31:16 +0800

    feat: init project

---
+++ src/code/Main.java
@@ -0,0 +1,11 @@
+package code;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        logger.log(Level.INFO, "Sample");
+    }
+}
